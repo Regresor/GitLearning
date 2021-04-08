ed17ba51395a782ceb329124e19158d781af516a
Regresor <52954435+Regresor@users.noreply.github.com>
2021-04-08 20:27:48 +0300

    Update Program.cs
    Хочу добавить сюда коментарий

--- GitLearning/Program.cs
+++ GitLearning/Program.cs
@@ -7,7 +7,7 @@ namespace GitLearning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            //Хочу добавить сюда коментарий
             Console.ReadKey();
         }
     }
