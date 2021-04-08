bbca9fdb29b1c877bd8b2c9ddf0208294be2230a
Regresor <52954435+Regresor@users.noreply.github.com>
2021-04-08 20:02:03 +0300

    Update Program.cs
    Add Console.ReadKey() function

--- GitLearning/Program.cs
+++ GitLearning/Program.cs
@@ -7,6 +7,8 @@ namespace GitLearning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
         }
     }
 }
