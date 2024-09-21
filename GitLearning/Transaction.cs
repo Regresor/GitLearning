9cf9b4bed1c1c1309d2511e6588b90b59be95c18
Regressor <artx80@gmail.com>
2024-09-21 19:18:47 +0300

    gitflow-feature-stash: new_transaction
    add transaction class

---
+++ GitLearning/Transaction.cs
@@ -0,0 +1,9 @@
+namespace GitLearning
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal TransferAmoutn { get; set; }
+        public decimal ReceiveAmount { get; set; }
+    }
+}
