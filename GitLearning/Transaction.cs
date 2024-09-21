4adaaf37d08200b2c57c85dd4ecbf8512dde4493
Regressor <artx80@gmail.com>
2024-09-21 19:56:46 +0300

    Revert "gitflow-feature-stash: new_transaction"
    This reverts commit 9cf9b4bed1c1c1309d2511e6588b90b59be95c18.

--- GitLearning/Transaction.cs
+++
@@ -1,9 +0,0 @@
-namespace GitLearning
-{
-    public class Transaction
-    {
-        public int Id { get; set; }
-        public decimal TransferAmoutn { get; set; }
-        public decimal ReceiveAmount { get; set; }
-    }
-}
